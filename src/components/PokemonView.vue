fc14dd80d09c7d997a87f995251001544684a9a2
pcezars <grimmpc@gmail.com>
2023-11-20 12:58:24 -0300

    modified:   src/components/PokemonView.vue

--- src/components/PokemonView.vue
+++ src/components/PokemonView.vue
@@ -11,12 +11,22 @@
     <div class="media">
         <div class="media-content">
             <p class="title is-4">{{ idFixo }} - {{ upper(name) }}</p>
-            <p class="subtitle is-6">{{ pokemon.type }}</p>
+            <hr>
+            <p class="subtitle is-6">Types:<strong v-for="(type) in pokemon.type" :key="type.type"><p>{{ upper(type.type.name) }}</p></strong></p>
+            <hr>
+            <p class="subtitle is-6">Base Stats:</p>
+            <p>HP: {{ baseHp }}</p>
+            <p>Attack: {{ baseAtk }}</p>
+            <p>Defense: {{ baseDef }}</p>
+            <p>Sp. Attack: {{ baseSpatk }}</p>
+            <p>Sp. Defense: {{ baseSpdef }}</p>
+            <p>Speed: {{ baseSpd }}</p>
+            <hr>
         </div>
     </div>
-
-    <div class="content">
-        <button class="button is-medium is-fullwidth" @click="mudarSprite">Mudar Sprite</button>
+    <div class="content buttons are-medium column is-full">
+        <button class="button is-responsive" @click="mudarSprite">Normal</button>
+        <button class="button is-responsive" @click="mudarShiny">Shiny</button>
     </div>
   </div>
 </div>
@@ -32,22 +42,43 @@ import axios from 'axios';
 export default{
     created: function(){
         axios.get(this.url).then(res =>{
-            this.pokemon.type = res.data.types[0].type.name;
+            this.pokemon.type = res.data.types;
             this.pokemon.front = res.data.sprites.front_default;
             this.pokemon.back = res.data.sprites.back_default;
+            this.pokemon.frontShiny = res.data.sprites.front_shiny;
+            this.pokemon.backShiny = res.data.sprites.back_shiny;
             this.currentImg = this.pokemon.front;
             this.idFixo = res.data.id;
+            
+            // stats
+
+            this.baseHp = res.data.stats[0].base_stat;
+            this.baseAtk = res.data.stats[1].base_stat;
+            this.baseDef = res.data.stats[2].base_stat;
+            this.baseSpatk = res.data.stats[3].base_stat;
+            this.baseSpdef = res.data.stats[4].base_stat;
+            this.baseSpd =  res.data.stats[5].base_stat;
+
         })
     },
     data(){
         return{
             isFront: true,
+            isShiny: false,
             currentImg: '',
             pokemon: {
                 type: '',
                 front: '',
+                frontShiny: '',
+                backShiny: '',
                 back: '',
-                idFixo: ''
+                idFixo: '',
+                baseHp: '',
+                baseAtk: '',
+                baseDef: '',
+                baseSpatk: '',
+                baseSpdef: '',
+                baseSpd: ''
             }
         }
     },
@@ -62,14 +93,26 @@ export default{
             return newName;
         },
         mudarSprite: function(){
-            if(this.isFront){
+            this.isShiny = false;
+                if(this.isFront){
                 this.isFront = false;
                 this.currentImg = this.pokemon.back;
-            } else {
-                this.isFront = true;
-                this.currentImg = this.pokemon.front;
+                } else {
+                    this.isFront = true;
+                    this.currentImg = this.pokemon.front;
+                }
+            
+        },
+        mudarShiny: function(){
+            this.isShiny = true;
+                if(this.isFront){
+                this.isFront = false;
+                this.currentImg = this.pokemon.backShiny;
+                } else {
+                    this.isFront = true;
+                    this.currentImg = this.pokemon.frontShiny;
+                }
             }
-        }
     }
 }
 </script>
